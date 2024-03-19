2ce00b636f6e59a26828495c88348b1e7a6cc49b
baranskas <martynasbaranskas@gmail.com>
2024-03-20 00:17:54 +0200

    Minor update
    -> Added timers for each customer, implementing functionality later.
    -> Working on game design by implementing new UI buttons.

--- src/game.py
+++ src/game.py
@@ -1,16 +1,42 @@
+import random
 from src import load_save
 from nicegui import app, ui
+from faker import Faker
+
+fake = Faker('en_US')
+
+dialogues = [
+    'I want something sweet and spicy. Maybe Curry Chicken?',
+    'Get me 2 glasses of wine and a cheese plate please.',
+    'I want something from the breakfast menu. May I get some pancakes with maple syrup?',
+    'My taste buds crave something sweet. May I get chocolate pancakes?',
+    '2 beers for me.'
+    ]
+
+
+class Customer:
+    def __init__(self):
+        self.rating = random.randint(0, 10)
+        self.name = fake.name()
+        self.order = dialogues[random.randint(0, len(dialogues) - 1)]
+
 
 # to do
 # - loading of restaurant [X]
 # - optimized code readability with modules and packages [X]
 
+
 RESTAURANT_ID = "Dev"
 
 
-def add_customer_card(name, column):
-    with ui.card().classes('w-[88.7vw] py-2') as customer_card:
-        ui.markdown(f"*{name}*").classes('text-h5 text-center')
+def add_customer_card(column):
+    customer = Customer()
+    with ui.card().classes('w-[88.7vw] py-2 justify-center') as customer_card:
+        with ui.row():
+            ui.circular_progress().value = 1
+            ui.markdown(f"{customer.rating}⭐").classes('text-h6')
+            ui.markdown(f"**{customer.name}**").classes('text-h5')
+            ui.markdown(f"*{customer.order}*").classes('text-h5')
 
     customer_card.move(column)
 
@@ -25,17 +51,17 @@ def game_page():
         ui.markdown(f"Restaurant **{restaurant['name']}**").classes('text-h5 text-center')
 
     with ui.row(wrap=False).classes('gap-2'):
-        with ui.card().classes('h-[90.6vh] w-[10vw]'):
+        with ui.card().classes('h-[90.6vh] w-[10vw] flex'):
             ui.markdown(f"**Money:** {restaurant['money']}").bind_content_from(restaurant, 'money', backward=lambda m: f"**Money:** {m}")
             ui.markdown(f"**Level:** {restaurant['level']}")
             ui.markdown(f"**Customers:** {restaurant['customers']}")
             ui.markdown(f"R_ID: *{restaurant['restaurant_id']}*")
 
-            ui.button("Add da money", on_click=lambda: restaurant.update(money=restaurant['money']+10000)).props('rounded color=primary').classes('w-full')
-            ui.button("Add Customer", on_click=lambda: add_customer_card("default", customer_column)).props('rounded color=primary').classes('w-full')
-            ui.button("Exit", on_click=lambda: ui.navigate.to(load_save.main_page)).props('rounded color=primary').classes('w-full')
+            ui.button("Kitchen").props('rounded color=primary').classes('w-full')
+            ui.button("Upgrades").props('rounded color=primary').classes('w-full')
+            ui.button("Exit", on_click=lambda: ui.navigate.to(load_save.main_page)).props('rounded color=primary').classes('self-end w-full')
 
         customer_column = ui.column().classes('gap-2')
         with customer_column:
             for i in range(0, 12):
-                add_customer_card("default", customer_column)
+                add_customer_card(customer_column)
